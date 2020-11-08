6e3a446e8bbcecd40895b8df9242408ee487cf91
Kartik Nema <knem7554@gmail.com>
2020-11-08 19:06:27 +0530

    classification

--- classify.py
+++ classify.py
@@ -1,36 +1,25 @@
 import pandas as pd
-
-df = pd.read_csv('m14_merged.csv', index_col=0)
-
-X = df.drop('label', axis=1).values
-y = df['label'].values
 from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-
-from sklearn.preprocessing import StandardScaler
-
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-
-lda = LDA(n_components=10)
-X_train = lda.fit_transform(X_train, y_train)
-X_test = lda.transform(X_test)
-
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
-
-classifier = RandomForestClassifier(max_depth=4, random_state=0)
-
-classifier.fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
-from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-print('Accuracy' + str(accuracy_score(y_test, y_pred)))
+
+data = pd.read_csv('data_set.csv', index_col=0)
+
+X = data.drop(['label','subject'], axis=1).values
+y = data['label'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+
+classifier = LinearRegression()
+classifier.fit(X_train,y_train)
+y_pred = classifier.predict(X_test)
+print(classifier.score(X_test,y_test))
+
+
+classifier = RandomForestClassifier(max_depth=4, random_state=0)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+print(str(accuracy_score(y_test, y_pred)))
